0cf48e111f419d07a3402e06753a2d546789bf47
Stephen Taylor <staylorx@gmail.com>
2026-01-12 23:54:58 -0700

    refactor: Remove logging-related classes and interfaces for cleanup

--- lib/src/domain/services/loggable.dart
+++
@@ -1,7 +0,0 @@
-import 'package:resumesux_domain/resumesux_domain.dart';
-
-/// Mixin that provides an optional logger for injection into repositories and use cases.
-mixin Loggable {
-  /// The optional logger instance.
-  Logger? logger;
-}
--- lib/src/domain/services/logger_extension.dart
+++
@@ -1,7 +0,0 @@
-import 'package:resumesux_domain/resumesux_domain.dart';
-
-extension LoggerExtension on Logger {
-  void log<T>(String method, String message) {
-    info('${T.toString()}.$method: $message');
-  }
-}
