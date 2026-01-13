2d34e48410eb1208a37356c3ac54a45565b505b2
Stephen Taylor <staylorx@gmail.com>
2026-01-12 17:01:38 -0700

    feat: Remove unused use cases for application generation and saving, cover letter, resume, and feedback

--- lib/src/domain/usecases/usecases.dart
+++ lib/src/domain/usecases/usecases.dart
@@ -2,7 +2,6 @@
 library;
 
 export 'get_digest_usecase.dart';
-export 'generate_and_save_application_usecase.dart';
 export 'generate_application_usecase.dart';
 export 'get_config_usecase.dart';
 export 'get_applicant_usecase.dart';
@@ -12,9 +11,4 @@ export 'generate_feedback_usecase.dart';
 export 'create_job_req_usecase.dart';
 export 'update_job_req_usecase.dart';
 export 'get_job_req_usecase.dart';
-export 'create_application_directory_usecase.dart';
 export 'save_ai_responses_usecase.dart';
-
-export 'save_resume_usecase.dart';
-export 'save_cover_letter_usecase.dart';
-export 'save_feedback_usecase.dart';
